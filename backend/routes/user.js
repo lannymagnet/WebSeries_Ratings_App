620b0adeac7e955076437b1d79bd02e84dcef770
lannymagnet <bharatdave00@gmail.com>
2024-03-17 18:47:39 +0530

    Validator added for user details.
    Added validator for the user to show proper error messages using express-validator middleware.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createUser } = require("../controller/user");
+const { validate, userValidator } = require("../middleware/validators");
 
 const router = express.Router();
 
-router.post("/create", createUser);
+router.post("/create", userValidator, validate, createUser);
 
 module.exports = router;
