5152868e1b62282bce719e98f3afa91bcbb7e81a
lannymagnet <bharatdave00@gmail.com>
2024-03-17 18:30:45 +0530

    Password Hashing and Unique Email
    Added password hashing using bcrypt. Api call now returns error message if the email is already in use by someone else.

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -3,6 +3,11 @@ const User = require("../models/user");
 exports.createUser = async (req, res) => {
   const { name, email, password } = req.body;
 
+  //Finding if the new user is unique or not.
+  const oldUser = await User.findOne({ email });
+
+  if (oldUser) return res.status(401).json({ error: "This email is already in use." });
+
   const newUser = new User({ name, email, password });
   await newUser.save();
 
